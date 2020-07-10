cdb93036ab42822ebaf0266953e74c6d72c387c8
kcharris <kcharris77@gmail.com>
2020-07-10 16:00:44 -0500

    add button to add Items to listOfItems array

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import Item from './Item'
 
 class ItemList extends React.Component{
+  constructor(props){
+    super(props)
+    setState = {
+      listOfItems: [],
+    }
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+  handleSubmit(){
+    this.setState({
+      listOfItems: listOfItems.concat([<Item/>])
+    })
+  }
   render(){
     return (
-      <div>Placeholder</div>
+      <form onSubmit={this.handleSubmit()}>
+        <input type="submit" value="Add Item"/>
+      </form>
     )
   }
 }
