2165efae740431b5307dc1c9d8594910f6018953
kcharris <kcharris77@gmail.com>
2020-07-10 12:00:08 -0500

    add ItemList and Item components with small amount of starter code

---
+++ src/components/Item.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class Item extends React.component {
+  constructor(props){
+    super(props)
+    setState = {
+      name: "",
+      order: null,
+      time: 0,
+      description: "",
+    }
+  }
+}
+
+export default Item
---
+++ src/components/ItemList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class ItemList extends React.Component{
+  render(){
+    return (
+      <div>Placeholder</div>
+    )
+  }
+}
+
+export default ItemList
