f481ca39633569dac3bc4a288befe2ef903d6876
kevin <kcharris77@gmail.com>
2020-07-12 03:12:11 -0500

    Add a incrementing value to serve as key to items in list

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -6,12 +6,14 @@ class ItemList extends React.Component{
     super(props)
     this.state = {
       listOfItems: [],
+      keyTracker: 0, //value that incrememnts as items are added, not deleted
     }
     this.handleClick = this.handleClick.bind(this)
   }
   handleClick(){
     this.setState({
-      listOfItems: this.state.listOfItems.concat(<Item/>)
+      listOfItems: this.state.listOfItems.concat(<Item key={keyTracker}/>),
+      keyTracker: (this.state.keyTracker + 1)
     })
   }
   render(){
