542693de5779550fb5eb1204b7d77fe691e07b68
kevin <kcharris77@gmail.com>
2020-07-19 05:16:11 -0500

    remove keyTracker and related statements.

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -6,14 +6,12 @@ class ItemList extends React.Component{
     super(props)
     this.state = {
       listOfItems: [],
-      keyTracker: 0, //value that incrememnts as items are added, not deleted
     }
     this.handleClick = this.handleClick.bind(this)
   }
   handleClick(){
     this.setState({
-      listOfItems: this.state.listOfItems.concat(<Item key={keyTracker}/>),
-      keyTracker: (this.state.keyTracker + 1)
+      listOfItems: this.state.listOfItems.concat(<Item/>),
     })
   }
   render(){
