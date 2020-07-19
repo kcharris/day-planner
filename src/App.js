7407efe04d1a58b7b7ecf646d485fd2ef4f5092b
kevin <kcharris77@gmail.com>
2020-07-19 05:13:48 -0500

    change app component to class

--- src/App.js
+++ src/App.js
@@ -2,13 +2,16 @@ import React from 'react';
 import TimeOverview from './components/TimeOverview'
 import ItemList from './components/ItemList'
 
-function App() {
-  return (
+class App extends React.Component {
+  render(){
+    return (
     <div>
       <TimeOverview />
       <ItemList />
     </div>
-  );
+    );
+  }
+  
 }
 
 export default App;
