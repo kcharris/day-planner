bd2c15daac23b07fef9ce979060ccb74ff5f5581
kevin <kcharris77@gmail.com>
2020-07-20 03:03:52 -0500

    add state to Item change input names and change function to use state

--- src/components/Item.js
+++ src/components/Item.js
@@ -3,6 +3,13 @@ import React from 'react'
 class Item extends React.Component {
   constructor(props){
     super(props)
+    this.state = {
+      name: '',
+      order: null,
+      hours: null,
+      minutes: null,
+      description: "",
+    }
     this.handleChange = this.handleChange.bind(this)
   }
   handleChange(e){
@@ -14,11 +21,11 @@ class Item extends React.Component {
   render(){
     return(
       <div>
-        <input type= "text" name='name' maxLength="20" value={this.state.name} onChange={this.props.handleChange}/>
-        <input type= 'number' name='order' min='0' max='100' value={this.state.order} onChange={this.props.handleChange}/>
-        <input type= 'number' name='Hrs' min='0' max='23' value={this.state.hours} onChange={this.props.handleChange}/>
-        <input type= 'number' name='Mins' min='0' max='59' value={this.state.minutes} onChange={this.props.handleChange}/>
-        <input type= 'textbox' size="40" name='description' value={this.state.description} onChange={this.props.handleChange}/>
+        <input type= "text" name='name' maxLength="20" value={this.state.name} onChange={this.state.handleChange}/>
+        <input type= 'number' name='order' min='0' max='100' value={this.state.order} onChange={this.state.handleChange}/>
+        <input type= 'number' name='hours' min='0' max='23' value={this.state.hours} onChange={this.state.handleChange}/>
+        <input type= 'number' name='minutes' min='0' max='59' value={this.state.minutes} onChange={this.state.handleChange}/>
+        <input type= 'textbox' size="40" name='description' value={this.state.description} onChange={this.state.handleChange}/>
       </div>
     )
   }
