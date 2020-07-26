a22fc2cfbfc1dd01aa16e1b019fda55254bdce94
kevin <kcharris77@gmail.com>
2020-07-26 02:49:50 -0500

    correct function calls by removing state

--- src/components/Item.js
+++ src/components/Item.js
@@ -21,11 +21,11 @@ class Item extends React.Component {
   render(){
     return(
       <div>
-        <input type= "text" name='name' maxLength="20" value={this.state.name} onChange={this.state.handleChange}/>
-        <input type= 'number' name='order' min='0' max='100' value={this.state.order} onChange={this.state.handleChange}/>
-        <input type= 'number' name='hours' min='0' max='23' value={this.state.hours} onChange={this.state.handleChange}/>
-        <input type= 'number' name='minutes' min='0' max='59' value={this.state.minutes} onChange={this.state.handleChange}/>
-        <input type= 'textbox' size="40" name='description' value={this.state.description} onChange={this.state.handleChange}/>
+        <input type= "text" name='name' maxLength="20" value={this.state.name} onChange={this.handleChange}/>
+        <input type= 'number' name='order' min='0' max='100' value={this.state.order} onChange={this.handleChange}/>
+        <input type= 'number' name='hours' min='0' max='23' value={this.state.hours} onChange={this.handleChange}/>
+        <input type= 'number' name='minutes' min='0' max='59' value={this.state.minutes} onChange={this.handleChange}/>
+        <input type= 'textbox' size="40" name='description' value={this.state.description} onChange={this.handleChange}/>
       </div>
     )
   }
