c76b3f72857b75b07040d9e4f5851da51b66722d
kevin <kcharris77@gmail.com>
2020-07-28 02:43:14 -0500

    add id to keep track of items and their data as well as lift the handleChange function in Item component

--- src/App.js
+++ src/App.js
@@ -16,9 +16,22 @@ class App extends React.Component {
         minutes: null,
         hours: null,
         description: '',
+        id: 0,
       }
     }
     this.handleClick = this.handleClick.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+  }
+  handleChange(e) {
+    this.setState({
+      listOfData: this.state.listOfData.map((x) => {
+        if (x.id == e.target.id){
+          x[e.target.name] = e.target.value
+        }
+        return x
+      }),
+      listOfItems: this.state.listOfData.map((x) => <Item data={x} onChange={this.handleChange}/>)
+    })
   }
   handleClick(){
     const data = Object.assign({}, this.state.dataObj)
@@ -27,9 +40,10 @@ class App extends React.Component {
       dataObj: (() => {
         let newObj = this.state.dataObj;
         newObj.order += 1
+        newObj.id += 1
         return newObj
       })(),
-      listOfItems: this.state.listOfData.map((x) => <Item data={x}/>),
+      listOfItems: this.state.listOfData.map((x) => <Item data={x} onChange={this.handleChange}/>),
     })
   }
   render(){
--- src/components/Item.js
+++ src/components/Item.js
@@ -1,25 +1,15 @@
 import React from 'react'
 
 class Item extends React.Component {
-  constructor(props){
-    super(props)
-    
-    this.handleChange = this.handleChange.bind(this)
-  }
-  handleChange(e){
-    this.setState({
-      [e.target.name]: e.target.value
-      }
-    )
-  }
+  
   render(){
     return(
       <div>
-        <input type= "text" name='name' maxLength="20" value={this.props.data.name} onChange={this.handleChange}/>
-        <input type= 'number' name='order' min='0' max='100' value={this.props.data.order} onChange={this.handleChange}/>
-        <input type= 'number' name='hours' min='0' max='23' value={this.props.data.hours} onChange={this.handleChange}/>
-        <input type= 'number' name='minutes' min='0' max='59' value={this.props.data.minutes} onChange={this.handleChange}/>
-        <input type= 'textbox' size="40" name='description' value={this.props.data.description} onChange={this.handleChange}/>
+        <input type= "text" name='name' maxLength="20" id={this.props.data.id} value={this.props.data.name} onChange={this.props.onChange}/>
+        <input type= 'number' name='order' min='0' max='100' id={this.props.data.id} value={this.props.data.order} onChange={this.props.onChange}/>
+        <input type= 'number' name='hours' min='0' max='23' id={this.props.data.id} value={this.props.data.hours} onChange={this.props.onChange}/>
+        <input type= 'number' name='minutes' min='0' max='59' id={this.props.data.id} value={this.props.data.minutes} onChange={this.props.onChange}/>
+        <input type= 'textbox' size="40" name='description' id={this.props.data.id} value={this.props.data.description} onChange={this.props.onChange}/>
       </div>
     )
   }
