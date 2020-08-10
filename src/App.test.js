1ec9ce47be6c861e317bb357eb62f4d740e72219
kevin <kcharris77@gmail.com>
2020-08-10 05:29:38 -0500

    add test to check value of Add Item button

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,33 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import {render, unmountComponentAtNode} from 'react-dom';
 import App from './App';
+import Item from './components/Item';
+import {act} from 'react-dom/test-utils';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+let container = null
+let addItemButton = null
+beforeEach(() => {
+  container = document.createElement('div')
+  document.body.appendChild(container)
+  act(() => {
+    render(<App />, container)
+    
+  })    
+  addItemButton = document.querySelector("input[type=button]");
+  act(() => {
+    for(let i = 1; i < 21; i++){
+      addItemButton.dispatchEvent(new MouseEvent('click', { bubbles: true }))
+    }
+  })
+  
+})
+afterEach(() => {
+  unmountComponentAtNode(container)
+  container.remove()
+  container = null
+  addItemButton = null
+})
+
+it ('creates a new item on button click', () => {
+  expect(addItemButton.value).toBe('Add Item')
+})
