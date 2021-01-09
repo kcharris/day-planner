eb9b36c0c3ea1b467df66410ee797373b5ec2899
Kevin <kcharris77@gmail.com>
2021-01-08 21:33:47 -0600

    remake json files and add new build

---
+++ build/precache-manifest.d1dbbb8de6ff838e60445ede4d8b3cbd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9a20518f2ded20cc9e42e593f91ad7b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60646cfc45e370e62e03",
+    "url": "/static/js/2.0c3cf0be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0c3cf0be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ebacf61ce4663ee3e2ca",
+    "url": "/static/js/main.97972102.chunk.js"
+  },
+  {
+    "revision": "6dfaf059d91e26b36dd2",
+    "url": "/static/js/runtime-main.4f322694.js"
+  }
+]);
